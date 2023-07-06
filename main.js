e1776bf9f3de2318e3a803624c5062a59b68dd53
codegrann <omondihills02@gmail.com>
2023-07-06 22:33:53 +0300

    toggled unselected dropdown to hide from view

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-let display=document.querySelector('#display');
-let content=document.querySelectorAll(".content");
-let titles=document.querySelectorAll(".title");
-let subTitles=document.querySelectorAll(".subTitle");
+let display = document.querySelector("#display");
+let content = document.querySelectorAll(".content");
+let titles = document.querySelectorAll(".title");
+let subTitles = document.querySelectorAll(".subTitle");
 
 // display.addEventListener('click', changeColor);
 // function changeColor(){
-    // display.style.backgroundColor='red';
+// display.style.backgroundColor='red';
 // }
 
 // toggle the dropdown to show
-titles.forEach((title)=>{
-    title.addEventListener('click', ()=>{
-        let dropdown=title.nextElementSibling;
-        if(dropdown.style.display=='block'){
-            dropdown.style.display='none';
-        } else{
-            dropdown.style.display='block';
+titles.forEach((title) => {
+  title.addEventListener("click", () => {
+    let dropdown = title.nextElementSibling;
+    let isOpen = dropdown.style.display == "block";
+    
+    titles.forEach((nonSelectedTitle)=>{
+        let closedDropdown=nonSelectedTitle.nextElementSibling;
+        if(closedDropdown!=dropdown){
+            closedDropdown.style.display='none';
         }
     })
-})
 
+    dropdown.style.display = isOpen ? "none" : "block";
+  });
+});
 
 // show each section's content
-subTitles.forEach((subTitle)=>{
-    subTitle.addEventListener('click', ()=>{
-        // console.log(subTitle.nextElementSibling)
-        display.innerHTML=subTitle.nextElementSibling.textContent;
-    })
-})
+subTitles.forEach((subTitle) => {
+  subTitle.addEventListener("click", () => {
+    // console.log(subTitle.nextElementSibling)
+    display.innerHTML = subTitle.nextElementSibling.textContent;
+  });
+});
