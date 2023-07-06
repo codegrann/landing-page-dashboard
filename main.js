64f913d99ea5d5211f735a2a72f86cbc199a27be
codegrann <omondihills02@gmail.com>
2023-07-06 21:03:57 +0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 let content=document.querySelector('#content');
 let sub=document.querySelector("#sub");
+let subb=document.querySelectorAll(".subb");
 
 content.addEventListener('click', changeColor);
 
+console.log(subb)
+
 function changeColor(){
     content.style.backgroundColor='red';
-    content.innerHTML=sub;
+    content.innerHTML=sub.innerHTML;
 }
+function adjust(){
+    content.innerHTML=el;
+}
+
+subb.forEach((el)=>{
+    el.addEventListener('click', adjust);
+})
